7138ac770921df7ace0ce4adb6c2924cf2f89f2d
Tim Hanewich <tahanewich@live.com>
2021-10-27 20:57:20 -0400

    Initial commit

---
+++ Learning/MainActivity.cs
@@ -0,0 +1,57 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Runtime;
+using AndroidX.AppCompat.App;
+using Android.Widget;
+using System;
+
+namespace Learning
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            Xamarin.Essentials.Platform.Init(this, savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            //Set up
+            MyTextView = FindViewById<TextView>(Resource.Id.MyText);
+            MyValue = 0;
+            ButtonIncrement = FindViewById<Button>(Resource.Id.PleaseIncrement);
+            ButtonDecrement = FindViewById<Button>(Resource.Id.PleaseDecrement);
+
+            //Assign
+            ButtonIncrement.Click += Inc;
+            ButtonDecrement.Click += Dec;
+
+        }
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        {
+            Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+
+            base.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
+        public int MyValue;
+        public TextView MyTextView;
+        public Button ButtonIncrement;
+        public Button ButtonDecrement;
+        
+
+        public void Inc(object sender, EventArgs e)
+        {
+            MyValue = MyValue + 1;
+            MyTextView.Text = MyValue.ToString("#,##0");
+        }
+
+        public void Dec(object sender, EventArgs e)
+        {
+            MyValue = MyValue - 1;
+            MyTextView.Text = MyValue.ToString("#,##0");
+        }
+    
+    }
+}
